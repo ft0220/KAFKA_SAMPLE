840a9be5246643346cf1dd8108f5276f6b320de9
ft0220 <kusabinagoru109072@gmail.com>
2019-11-13 00:00:05 +0900

    test追加

--- src/main/java/com/kafka/sample/kafka_sample/Consumer/Consumer.java
+++ src/main/java/com/kafka/sample/kafka_sample/Consumer/Consumer.java
@@ -1,5 +1,7 @@
 package com.kafka.sample.kafka_sample.Consumer;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -11,11 +13,23 @@ public class Consumer{
      //log出力用
      private static final Logger LOGGER=LoggerFactory.getLogger(Consumer.class);
 
+
+
+     private CountDownLatch latch = new CountDownLatch(1);
+
+
+
+     public CountDownLatch getLatch() {
+   
+       return latch;
+   
+     }
     //kafkaListenerによってどのTopicの情報を取得するのか、
     //どのgroupIdの情報を取得するのかを設定でき、エラーが発生した場合のハンドリングなども可能。
     @KafkaListener(topics = "test",groupId = "sample")
     public void consumer(String message) {
         LOGGER.info("received message is {}",message);
+        latch.countDown();
     }
 
 }
--- src/test/java/com/kafka/sample/kafka_sample/DemoApplicationTests.java
+++ src/test/java/com/kafka/sample/kafka_sample/DemoApplicationTests.java
@@ -1,16 +1,52 @@
 package com.kafka.sample.kafka_sample;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import com.kafka.sample.kafka_sample.Consumer.Consumer;
+import com.kafka.sample.kafka_sample.Producer.Producer;
+
+import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DirtiesContext
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
 
+
+  @Autowired
+  private Consumer receiver;
+  @Autowired
+  private Producer producer;
+
+
+
+  @Test
+  public void testReceive() throws Exception {
+    // send the message
+    String greeting = "Hello Spring Kafka Receiver!";
+    producer.sendKafka(greeting);
+    receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+    // check that the message was received
+    assertThat(receiver.getLatch().getCount()).isEqualTo(0);
+  }
 }
+
+
